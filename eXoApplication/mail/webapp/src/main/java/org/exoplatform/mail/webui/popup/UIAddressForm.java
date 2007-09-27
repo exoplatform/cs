473a6e2dc601734ecb62e39ad66eb7fb797854c4
John Doe <noreply@exoplatform.com>
2007-09-27 02:31:33 +0000

    SVM: skipping changes 20202-20203 for svn://svn.forge.objectweb.org/svnroot/exoplatform/projects

--- eXoApplication/mail/webapp/src/main/java/org/exoplatform/mail/webui/popup/UIAddressForm.java
+++ eXoApplication/mail/webapp/src/main/java/org/exoplatform/mail/webui/popup/UIAddressForm.java
@@ -6,7 +6,16 @@ package org.exoplatform.mail.webui.popup;
 
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.collections.map.HashedMap;
+import org.exoplatform.mail.service.Message;
 import org.exoplatform.mail.webui.UIMailPortlet;
+import org.exoplatform.portal.webui.util.Util;
+import org.exoplatform.contact.service.*;
 import org.exoplatform.services.jcr.util.IdGenerator;
 import org.exoplatform.upload.UploadResource;
 import org.exoplatform.upload.UploadService;
@@ -19,6 +28,7 @@ import org.exoplatform.webui.event.Event;
 import org.exoplatform.webui.event.EventListener;
 import org.exoplatform.webui.event.Event.Phase;
 import org.exoplatform.webui.form.UIForm;
+import org.exoplatform.webui.form.UIFormCheckBoxInput;
 import org.exoplatform.webui.form.UIFormUploadInput;
 
 import com.thoughtworks.xstream.core.ReferenceByIdMarshaller.IDGenerator;
@@ -39,17 +49,52 @@ import com.thoughtworks.xstream.core.ReferenceByIdMarshaller.IDGenerator;
 )
 
 public class UIAddressForm extends UIForm implements UIPopupComponent { 
-  public UIAddressForm() throws Exception {  }
+  public UIAddressForm() throws Exception {  
+    setContactList("");
+  }
+  private Map<String, Contact> contactMap_ = new HashMap<String, Contact>(); 
 
   public void activate() throws Exception {}
   public void deActivate() throws Exception {}
+  
+  public List<Contact> getContacts() throws Exception { 
+    return new ArrayList<Contact>(contactMap_.values());
+  }
+  
+  
+  public void setContactList() throws Exception {
+    setContactList("");
+  }
 
+  public void setContactList(String groupId) throws Exception {
+    List<Contact> contacts = new ArrayList<Contact>();
+    ContactService contactSrv = getApplicationComponent(ContactService.class);
+    String username = Util.getPortalRequestContext().getRemoteUser();   
+    if (groupId == null || groupId == "") {
+      contacts = contactSrv.getAllContact(username);
+    } else {
+      contacts = contactSrv.getContactsByGroup(username, groupId);
+    }
+    setContactList(contacts);
+  }
+  
+  
+  public void setContactList(List<Contact> contactList) throws Exception {
+    getChildren().clear();
+    contactMap_.clear();
+    for (Contact contact : contactList) {
+      addUIFormInput(new UIFormCheckBoxInput<Boolean>(contact.getId(), contact.getId(), false));
+      contactMap_.put(contact.getId(), contact);
+    }
+  }
+  
   static  public class SaveActionListener extends EventListener<UIAddressForm> {
     public void execute(Event<UIAddressForm> event) throws Exception {
+    //  event.getSource().getAncestorOfType(UIMailPortlet.class). ;
      
     }
   }
-
+  
   static  public class CancelActionListener extends EventListener<UIAddressForm> {
     public void execute(Event<UIAddressForm> event) throws Exception {
       event.getSource().getAncestorOfType(UIMailPortlet.class).cancelAction() ;
