aa35d6cbd77487995146b4c0e2d8521a09f4e728
Pham Tuan <tuanp@exoplatform.com>
2007-11-29 08:30:14 +0000

    fix permission select

--- eXoApplication/calendar/webapp/src/main/java/org/exoplatform/calendar/webui/popup/UIGroupSelector.java
+++ eXoApplication/calendar/webapp/src/main/java/org/exoplatform/calendar/webui/popup/UIGroupSelector.java
@@ -165,10 +165,10 @@ public class UIGroupSelector extends UIGroupMembershipSelector implements UIPopu
     public void execute(Event<UIBreadcumbs> event) throws Exception {
       UIBreadcumbs uiBreadcumbs = event.getSource() ;
       UIGroupSelector uiGroupSelector = uiBreadcumbs.getParent() ;
-      String objectId =  event.getRequestContext().getRequestParameter(OBJECTID) ;
-      uiBreadcumbs.setSelectPath(objectId);     
-      String selectGroupId = uiBreadcumbs.getSelectLocalPath().getId() ;
-      uiGroupSelector.changeGroup(selectGroupId) ;
+      //String objectId =  event.getRequestContext().getRequestParameter(OBJECTID) ;
+      uiBreadcumbs.setSelectPath(uiGroupSelector.getCurrentGroup().getId());     
+      //String selectGroupId = uiBreadcumbs.getSelectLocalPath().getId() ;
+      uiGroupSelector.changeGroup(uiGroupSelector.getCurrentGroup().getId()) ;
       event.getRequestContext().addUIComponentToUpdateByAjax(uiGroupSelector) ;
     }
   }
