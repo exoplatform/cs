ea23008736e53ee1d02996f8393839eef401ac49
Brice Revenant <brice.revenant@exoplatform.com>
2007-12-05 15:50:49 +0000

    Preparing the branches. Refactored POMs and build scripts. Bonita configuration needs to be reintroduced as it was not implemented in a convenient way.

---
+++ module.js
@@ -0,0 +1,58 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+  var portal = params.portal;
+
+  var module = new Module();
+
+  module.version =  "trunk" ;
+  module.relativeMavenRepo =  "org/exoplatform/cs" ;
+  module.relativeSRCRepo =  "cs/trunk" ;
+  module.name = "cs" ;
+    
+  module.eXoApplication = {};
+  module.eXoApplication.mail = 
+    new Project("org.exoplatform.cs", "exo.cs.eXoApplication.mail.webapp", "war", module.version).
+    addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.mail.service", "jar",  module.version));
+  module.eXoApplication.mail.deployName = "mail";
+    
+  module.eXoApplication.forum = 
+    new Project("org.exoplatform.cs", "exo.cs.eXoApplication.forum.webapp", "war", module.version).
+      addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.forum.service", "jar",  module.version));
+  module.eXoApplication.forum.deployName = "forum";
+    
+  module.eXoApplication.calendar = 
+    new Project("org.exoplatform.cs", "exo.cs.eXoApplication.calendar.webapp", "war", module.version).
+      addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.calendar.service", "jar",  module.version)).
+      addDependency(new Project("ical4j", "ical4j", "jar", "0.9.20")) ;
+  module.eXoApplication.calendar.deployName = "calendar";
+    
+  module.eXoApplication.contact = 
+    new Project("org.exoplatform.cs", "exo.cs.eXoApplication.contact.webapp", "war", module.version).
+      addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.contact.service", "jar",  module.version)).
+      addDependency(new Project("net.wimpi.pim", "jpim-0.1", "jar",  "1.0"));
+  module.eXoApplication.contact.deployName = "contact";
+    
+  module.eXoApplication.content = 
+    new Project("org.exoplatform.cs", "exo.cs.eXoApplication.content.webapp", "war", module.version).
+      addDependency(new Project("org.exoplatform.cs", "exo.cs.eXoApplication.content.service", "jar",  module.version));
+  module.eXoApplication.content.deployName = "content";
+    
+  module.eXoApplication.web = {}
+  module.eXoApplication.web.csportal = 
+    new Project("org.exoplatform.cs", "exo.cs.eXoApplication.web.portal", "exo-portal", module.version).
+      addDependency(portal.web.eXoResources) .
+      addDependency(portal.web.eXoMacSkin) .
+      addDependency(portal.web.eXoVistaSkin) .
+      addDependency(portal.webui.portal) .
+      addDependency(jcr.frameworks.command) .
+      addDependency(jcr.frameworks.web) ;
+      
+  return module;
+}
