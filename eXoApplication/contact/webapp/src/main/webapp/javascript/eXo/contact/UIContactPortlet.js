5926331ccd5948f9bed853e7ba303e967ed5d154
Dung Hoang <dung.hoang@exoplatform.com>
2008-01-05 08:16:30 +0000

    correct addressbook context menu again

--- eXoApplication/contact/webapp/src/main/webapp/javascript/eXo/contact/UIContactPortlet.js
+++ eXoApplication/contact/webapp/src/main/webapp/javascript/eXo/contact/UIContactPortlet.js
@@ -60,28 +60,44 @@ UIContactPortlet.prototype.contactCallback = function(evt) {
 
 UIContactPortlet.prototype.addressBookCallback = function(evt) {
 	var UIContextMenu = eXo.webui.UIContextMenu ;
+	var DOMUtil = eXo.core.DOMUtil ;
 	var _e = window.event || evt ;	
 	var src = _e.srcElement || _e.target ;
-	var a = (src.nodeName.toLowerCase() == "a") ? src : eXo.core.DOMUtil.findFirstChildByClass(src, "a", "IconHolder") ;	
+	var a = (DOMUtil.hasClass(src, "ItemList")) ? src : DOMUtil.findAncestorByClass(src, "ItemList") ;	
 	eXo.webui.UIContextMenu.changeAction(UIContextMenu.menuElement, a.id) ;
-	var isPublic = a.getAttribute("isPublic") ;
-	var DOMUtil = eXo.core.DOMUtil ;
+	var isPublic = a.getAttribute("addressType") ;
 	var menuItems = DOMUtil.findDescendantsByClass(UIContextMenu.menuElement, "div", "ItemIcon") ;
 	var itemLength = menuItems.length ;
-	if (isPublic && (isPublic.toLowerCase() == "true")) {
+	if (isPublic && (isPublic.toLowerCase() == "2")) {
 		for(var i = 0 ; i < itemLength ; i ++) {
 			if (DOMUtil.hasClass(menuItems[i],"ShareIcon") || DOMUtil.hasClass(menuItems[i],"EditActionIcon") || DOMUtil.hasClass(menuItems[i],"DeleteIcon") || DOMUtil.hasClass(menuItems[i],"ImportAddressIcon") || DOMUtil.hasClass(menuItems[i],"ImportContactIcon")) {
-				if (menuItems[i].parentNode.getAttribute("oldHref")) break ;
+				if (menuItems[i].parentNode.getAttribute("oldHref")) continue ;
 				menuItems[i].parentNode.setAttribute("oldHref", menuItems[i].parentNode.href) ;
 				menuItems[i].parentNode.href = "javascript: void(0) ;" ;
 				menuItems[i].parentNode.setAttribute("oldColor", DOMUtil.getStyle(menuItems[i].parentNode, "color")) ;
 				menuItems[i].parentNode.style.color = "#cccccc" ;
 			}
 		}
-	} else {
+	} else if(isPublic.toLowerCase() == "1") {
+			for(var i = 0 ; i < itemLength ; i ++) {
+				if (DOMUtil.hasClass(menuItems[i],"ShareIcon") || DOMUtil.hasClass(menuItems[i],"ImportAddressIcon") || DOMUtil.hasClass(menuItems[i],"ImportContactIcon")) {
+					if (menuItems[i].parentNode.getAttribute("oldHref")) continue ;
+					menuItems[i].parentNode.setAttribute("oldHref", menuItems[i].parentNode.href) ;
+					menuItems[i].parentNode.href = "javascript: void(0) ;" ;
+					menuItems[i].parentNode.setAttribute("oldColor", DOMUtil.getStyle(menuItems[i].parentNode, "color")) ;
+					menuItems[i].parentNode.style.color = "#cccccc" ;
+				} else {
+					if (!menuItems[i].parentNode.getAttribute("oldHref")) continue ;
+					menuItems[i].parentNode.href = menuItems[i].parentNode.getAttribute("oldHref") ;
+					menuItems[i].parentNode.style.color = menuItems[i].parentNode.getAttribute("oldColor") ;
+					menuItems[i].parentNode.removeAttribute("oldColor") ;
+					menuItems[i].parentNode.removeAttribute("oldHref") ;
+				}
+			}
+	}	else {
 		for(var i = 0 ; i < itemLength ; i ++) {
 			if (DOMUtil.hasClass(menuItems[i],"ShareIcon") || DOMUtil.hasClass(menuItems[i],"EditActionIcon") || DOMUtil.hasClass(menuItems[i],"DeleteIcon") || DOMUtil.hasClass(menuItems[i],"ImportAddressIcon") || DOMUtil.hasClass(menuItems[i],"ImportContactIcon")) {
-				if (!menuItems[i].parentNode.getAttribute("oldHref")) break ;
+				if (!menuItems[i].parentNode.getAttribute("oldHref")) continue ;
 				menuItems[i].parentNode.href = menuItems[i].parentNode.getAttribute("oldHref") ;
 				menuItems[i].parentNode.style.color = menuItems[i].parentNode.getAttribute("oldColor") ;
 				menuItems[i].parentNode.removeAttribute("oldColor") ;
