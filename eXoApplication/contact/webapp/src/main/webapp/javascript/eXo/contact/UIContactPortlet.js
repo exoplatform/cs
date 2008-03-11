5322935cb0be15fd6994aa2b3872607c2c4f4802
Dung Hoang <dung.hoang@exoplatform.com>
2008-03-11 08:44:41 +0000

    fix bug CS-309

--- eXoApplication/contact/webapp/src/main/webapp/javascript/eXo/contact/UIContactPortlet.js
+++ eXoApplication/contact/webapp/src/main/webapp/javascript/eXo/contact/UIContactPortlet.js
@@ -488,6 +488,16 @@ UIContactPortlet.prototype.addCheckedIcon = function(layout) {
 } ;
 */
 
+UIContactPortlet.prototype.showImMenu = function(obj, event) {
+	var menuItems = eXo.core.DOMUtil.findDescendantsByClass(obj, "span", "MenuItem") ;
+	var len = menuItems.length ;
+	for(var i = 0 ; i < len ; i++) {
+		if (menuItems[i].style.display != "none") break ;
+	}
+	if (i == len) return ;
+	eXo.webui.UIPopupSelectCategory.show(obj, event) ;
+}
+
 /**
  * @author Lam Nguyen
  * 
