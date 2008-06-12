e5ee13b763d7932823f27b62f7163d2a5315ce79
Hung Quang Hoang <hung.hoang@exoplatform.com>
2008-06-12 08:09:12 +0000

    cs-917 fix bug when import contacts

--- eXoApplication/contact/service/src/main/java/org/exoplatform/contact/service/impl/VCardImportExport.java
+++ eXoApplication/contact/service/src/main/java/org/exoplatform/contact/service/impl/VCardImportExport.java
@@ -292,10 +292,11 @@ public class VCardImportExport implements ContactImportExport {
       String lastName = identity.getLastname();
       contact.setLastName(lastName);
       String firstName = identity.getFirstname();
-      contact.setFirstName(identity.getFirstname() + " " + additionName);
+
+      if (firstName.trim().equals(additionName.trim())) contact.setFirstName(firstName) ;
+      else contact.setFirstName(firstName + " " + additionName);
       
-      int size = identity.getAdditionalNameCount();
-      
+      int size = identity.getAdditionalNameCount();      
       String nickName = "";
       size = identity.getNicknameCount();
       for (int i = 0; i < size; i++) {
