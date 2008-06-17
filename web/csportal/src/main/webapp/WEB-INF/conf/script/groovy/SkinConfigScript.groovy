3c871fd5dced10fea3ee86a299497922f427dc92
Nguyen Quang Hung <hung.nguyen@exoplatform.com>
2008-06-17 07:50:09 +0000

    CS-946, CS-947: Changed picture of homepage and i18n homepage

---
+++ web/csportal/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -0,0 +1,44 @@
+/***************************HomePagePortlet**************************************************/
+SkinService.addSkin(
+    "csResources/CSHomePagePortlet",
+    "Default",
+    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "csResources/CSHomePagePortlet",
+    "Vista",
+    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+ 
+SkinService.addSkin(
+    "csResources/CSHomePagePortlet",
+    "Mac",
+    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+
+/***************************FooterPortlet**************************************************/
+SkinService.addSkin(
+    "csResources/CSFooterPortlet",
+    "Default",
+    "/portal/templates/skin/webui/component/UIFooterPortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "csResources/CSFooterPortlet",
+    "Vista",
+    "/portal/templates/skin/webui/component/UIFooterPortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+ 
+SkinService.addSkin(
+    "csResources/CSFooterPortlet",
+    "Mac",
+    "/portal/templates/skin/webui/component/UIFooterPortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
