bfdd2953ea4d6d36c29561e0deb26fa46ff4165b
Patrice Lamarque <patrice.lamarque@exoplatform.com>
2008-06-19 16:13:33 +0000

    adding demo portal project

---
+++ web/csdemo/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -0,0 +1,66 @@
+/***************************HomePageDemoPortlet**************************************************/
+SkinService.addSkin(
+    "csResources/CSHomePageDemoPortlet",
+    "Default",
+    "/portal/templates/skin/webui/component/UIHomePageDemoPortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "csResources/CSHomePageDemoPortlet",
+    "Vista",
+    "/portal/templates/skin/webui/component/UIHomePageDemoPortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+ 
+SkinService.addSkin(
+    "csResources/CSHomePageDemoPortlet",
+    "Mac",
+    "/portal/templates/skin/webui/component/UIHomePageDemoPortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+/***************************HomePagePortlet**************************************************/
+SkinService.addSkin(
+    "csResources/CSHomePagePortlet",
+    "Default",
+    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "csResources/CSHomePagePortlet",
+    "Vista",
+    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+ 
+SkinService.addSkin(
+    "csResources/CSHomePagePortlet",
+    "Mac",
+    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+
+/***************************FooterPortlet**************************************************/
+SkinService.addSkin(
+    "csResources/CSFooterPortlet",
+    "Default",
+    "/portal/templates/skin/webui/component/UIFooterPortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "csResources/CSFooterPortlet",
+    "Vista",
+    "/portal/templates/skin/webui/component/UIFooterPortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+ 
+SkinService.addSkin(
+    "csResources/CSFooterPortlet",
+    "Mac",
+    "/portal/templates/skin/webui/component/UIFooterPortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
