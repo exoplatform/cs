010e0868ed180bd68919da08bb15d3ac99817761
Dung Hoang <dung.hoang@exoplatform.com>
2008-08-06 02:46:54 +0000

    implement popup remider

--- eXoApplication/calendar/webapp/src/main/webapp/javascript/eXo/calendar/Reminder.js
+++ eXoApplication/calendar/webapp/src/main/webapp/javascript/eXo/calendar/Reminder.js
@@ -6,15 +6,15 @@ Reminder.prototype.init = function(eXoUser, eXoToken){
   eXo.core.Cometd.exoId = eXoUser;
   eXo.core.Cometd.exoToken = eXoToken;
   eXo.core.Cometd.subscribe('/eXo/Application/Calendar/messages', function(eventObj) {
-    eXo.calendar.Reminder.alarm(eventObj) ;
+		eXo.calendar.Reminder.alarm(eventObj) ;
   });
 	if (!eXo.core.Cometd.isConnected()) {
      eXo.core.Cometd.init();
   }
 } ;
 
-Reminder.prototype.alarm = function(){
-  //alert(eventObj) ;
+Reminder.prototype.alarm = function(eventObj){
+	var a = eXo.core.JSON.parse(eventObj);
 } ;
 
 eXo.calendar.Reminder = new Reminder() ;
