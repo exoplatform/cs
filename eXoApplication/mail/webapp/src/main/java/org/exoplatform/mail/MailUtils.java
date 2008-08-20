893ad74e962a1a126019bc56bfcabd76d37f213d
Nam Hai Phung <nam.phung@exoplatform.com>
2008-08-20 04:38:18 +0000

    Fixed CS-944 Bug unknown error when delete message of Trash folder in special case

--- eXoApplication/mail/webapp/src/main/java/org/exoplatform/mail/MailUtils.java
+++ eXoApplication/mail/webapp/src/main/java/org/exoplatform/mail/MailUtils.java
@@ -250,7 +250,7 @@ public class MailUtils {
     if (isFieldEmpty(s)) return "" ;
     s = decodeHTML(s);
     // for external link with form http:// , https://, ftp://
-    s = s.replaceAll("(https?|ftp)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]", "<a target=\"_blank\" href=\"$0\"> $0 </a>") ;
+    s = s.replaceAll("([^((href|src)=\")])(https?|ftp)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]", "<a target=\"_blank\" href=\"$0\"> $0 </a>") ;
     // for email 
     s = s.replaceAll("[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[_A-Za-z0-9-.]+\\.[A-Za-z]{2,5}", "<a target=\"_blank\" href=\"mailto:$0\"> $0 </a>") ;
     return s ;
