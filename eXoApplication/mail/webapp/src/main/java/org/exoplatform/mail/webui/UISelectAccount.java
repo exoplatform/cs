ee71bd9723ea4ec96b509c36503b7b24b996793e
Tuan  Pham <tuan.pham@exoplatform.com>
2008-08-22 09:47:44 +0000

    fix CS-1263 ui problem when edit acount

--- eXoApplication/mail/webapp/src/main/java/org/exoplatform/mail/webui/UISelectAccount.java
+++ eXoApplication/mail/webapp/src/main/java/org/exoplatform/mail/webui/UISelectAccount.java
@@ -139,7 +139,7 @@ public class UISelectAccount extends UIForm {
       } 
       UIMailPortlet uiPortlet = uiForm.getAncestorOfType(UIMailPortlet.class) ;
       UIPopupAction uiPopupAction = uiPortlet.getChild(UIPopupAction.class) ;
-      UIPopupActionContainer uiPopupContainer = uiPopupAction.activate(UIPopupActionContainer.class, 730) ;
+      UIPopupActionContainer uiPopupContainer = uiPopupAction.activate(UIPopupActionContainer.class, 800) ;
       uiPopupContainer.setId("UIAccountPopupSetting");
       UIAccountSetting uiAccountSetting = uiPopupContainer.createUIComponent(UIAccountSetting.class, null, null);
       uiPopupContainer.addChild(uiAccountSetting) ; 
