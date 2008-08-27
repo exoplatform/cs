aaa685fbbc297a4865e371f2ce83ddf2f827d407
Dung Hoang <dung.hoang@exoplatform.com>
2008-08-27 04:04:58 +0000

    add some utility method to use

--- web/csResources/src/main/webapp/javascript/eXo/cs/CSUtils.js
+++ web/csResources/src/main/webapp/javascript/eXo/cs/CSUtils.js
@@ -369,6 +369,79 @@ Utils.prototype.xml2json = function(xml, tab) {
 
 eXo.cs.Utils = new Utils() ;
 
+/********************* Event Manager ******************/
+
+function EventManager(){
+	
+}
+
+EventManager.prototype.addEvent = function( obj, type, fn ) {
+  if ( obj.attachEvent ) {
+    obj['e'+type+fn] = fn;
+    obj[type+fn] = function(){obj['e'+type+fn]( window.event );}
+    obj.attachEvent( 'on'+type, obj[type+fn] );
+  } else
+    obj.addEventListener( type, fn, false );
+};
+
+EventManager.prototype.removeEvent = function( obj, type, fn ) {
+  if ( obj.detachEvent ) {
+    obj.detachEvent( 'on'+type, obj[type+fn] );
+    obj[type+fn] = null;
+  } else
+    obj.removeEventListener( type, fn, false );
+};
+
+EventManager.prototype.removeEvent = function( obj, type, fn ) {
+  if ( obj.detachEvent ) {
+    obj.detachEvent( 'on'+type, obj[type+fn] );
+    obj[type+fn] = null;
+  } else
+    obj.removeEventListener( type, fn, false );
+};
+
+EventManager.prototype.getEventTarget = function(evt){
+	var evt = evt || window.event;
+	var target = evt.target || evt.srcElement;
+	if (target.nodeType == 3) { // check textNode
+		target = target.parentNode; 
+	}
+	return target; 
+};
+
+EventManager.prototype.getEventTargetByClass = function(evt, className){
+	var target = this.getEventTarget(evt);
+	if (eXo.core.DOMUtil.hasClass(target, className))
+		return target ;
+	else
+		return eXo.core.DOMUtil.findAncestorByClass(target, className) ;
+};
+
+EventManager.prototype.getEventTargetByTagName = function(evt, tagName){
+	var target = this.getEventTarget(evt);
+	if (target.tagName.toLowerCase() == tagName.trim())
+		return target ;
+	else
+		return eXo.core.DOMUtil.findAncestorByTagName(target, tagName) ;
+};
+
+EventManager.prototype.cancelBubble = function(evt) {
+  if(eXo.core.Browser.browserType == 'ie')
+    window.event.cancelBubble = true ;
+  else 
+    evt.stopPropagation() ;		  
+};
+
+EventManager.prototype.cancelEvent = function(evt) {
+	this.cancelBubble(evt) ;
+  if(eXo.core.Browser.browserType == 'ie')
+    window.event.returnValue = true ;
+  else
+    evt.preventDefault() ;
+};
+
+eXo.core.EventManager = new EventManager() ;
+
 /********************* Scroll Manager ******************/
 
 function UINavigation() {
@@ -412,3 +485,4 @@ UINavigation.prototype.scrollCallback = function() {
 } ;
 
 eXo.cs.UINavigation = new UINavigation() ;
+
