87237c9f5d30da1e5072e71f6834a18df790336c
Dung Hoang <dung.hoang@exoplatform.com>
2008-10-21 04:05:06 +0000

    clean code

--- web/csResources/src/main/webapp/javascript/eXo/cs/MaskLayerControl.js
+++ web/csResources/src/main/webapp/javascript/eXo/cs/MaskLayerControl.js
@@ -1,6 +1,66 @@
 /**
  * @author uocnb
  */
+//TODO : fix bug masklayer don't scroll with browser scrollbar(when show picture in cs and ks), remove this method when portal team fix it./
+eXo.core.UIMaskLayer.createMask = function(blockContainerId, object, opacity, position) {
+	try {
+		var Browser = eXo.core.Browser ;
+		var blockContainer = document.getElementById(blockContainerId) ;
+		var maskLayer = document.createElement("div") ;
+		
+		this.object = object ;
+		this.blockContainer = blockContainer ;
+		this.position = position ;
+		
+		if (document.getElementById("MaskLayer")) {
+			document.getElementById("MaskLayer").id = "subMaskLayer";
+		}
+		blockContainer.appendChild(maskLayer) ;
+		
+		maskLayer.className = "MaskLayer" ;
+		maskLayer.id = "MaskLayer" ;
+		maskLayer.maxZIndex = 3 ;
+		maskLayer.style.width = "100%"  ;
+		maskLayer.style.height = "100%" ;
+		maskLayer.style.top = "1px" ;
+		maskLayer.style.left = "0px" ;
+		maskLayer.style.zIndex = maskLayer.maxZIndex ;
+
+		if(opacity) {
+	    Browser.setOpacity(maskLayer, opacity) ;
+		}
+		
+		if(object != null){
+			if(object.nextSibling) {
+			  maskLayer.nextSiblingOfObject = object.nextSibling ;
+			  maskLayer.parentOfObject = null ;
+			} else {
+			  maskLayer.nextSiblingOfObject = null ;
+			  maskLayer.parentOfObject = object.parentNode ;
+			}
+			
+			object.style.zIndex = maskLayer.maxZIndex + 1 ;
+			object.style.display = "block" ;
+			
+			blockContainer.appendChild(object) ;
+		
+			eXo.core.UIMaskLayer.setPosition() ;
+			
+			if((blockContainer.offsetWidth > object.offsetLeft + object.offsetWidth) && (position == "TOP-RIGHT") || (position == "BOTTOM-RIGHT")) {
+		    object.style.left = blockContainer.offsetWidth - object.offsetWidth + "px" ;
+			}
+			eXo.core.UIMaskLayer.doScroll() ;
+	  }
+		if(maskLayer.parentNode.id == "UIPage") {
+			eXo.core.UIMaskLayer.enablePageDesktop(false);
+	  }
+	}catch(err) {
+		alert(err) ;
+	}
+	eXo.core.UIMaskLayer.objectTop = eXo.core.UIMaskLayer.object.offsetTop - document.documentElement.scrollTop;
+	return maskLayer ;
+};
+
 function MaskLayerControl() {
   this.domUtil = eXo.core.DOMUtil ;
 }
@@ -26,6 +86,7 @@ MaskLayerControl.prototype.showPictureWrapper = function() {
 MaskLayerControl.prototype.showPicture = function(node) {
   var attachmentContent = this.domUtil.findAncestorByClass(node, 'AttachmentContent') ;
   var imgSrcNode = this.domUtil.findDescendantsByClass(attachmentContent, 'img', 'AttachmentFile')[0] ;
+	this.isMail = this.domUtil.findAncestorByClass(node,"UIMailPortlet");
   var containerNode = document.createElement('div') ;
   with (containerNode.style) {
     margin = 'auto' ;
@@ -45,12 +106,16 @@ MaskLayerControl.prototype.showPicture = function(node) {
   containerNode.appendChild(imageNode) ;
   containerNode.setAttribute('title', 'Click to close') ;
   containerNode.onclick = this.hidePicture ;
-  maskNode = eXo.core.UIMaskLayer.createMask('UIPortalApplication', containerNode, 30, 'CENTER') ;
-  eXo.core.Browser.addOnScrollCallback('MaskLayerControl', this.scrollHandler) ;
+  var maskNode = eXo.core.UIMaskLayer.createMask('UIPortalApplication', containerNode, 30, 'CENTER') ;
+  eXo.core.Browser.addOnScrollCallback('MaskLayerControl', this.scrollHandler) ;	
 } ;
 
 MaskLayerControl.prototype.scrollHandler = function() {
-  eXo.core.UIMaskLayer.object.style.top = 20 + document.body.scrollTop + 'px' ;
+	if(eXo.cs.MaskLayerControl.isMail) {
+		eXo.core.UIMaskLayer.object.style.top = document.documentElement.scrollTop + 20 + 'px' ;
+		return ;
+	}
+  eXo.core.UIMaskLayer.object.style.top = eXo.core.UIMaskLayer.objectTop + document.documentElement.scrollTop + 'px' ;
 } ;
 
 MaskLayerControl.prototype.hidePicture = function() {
