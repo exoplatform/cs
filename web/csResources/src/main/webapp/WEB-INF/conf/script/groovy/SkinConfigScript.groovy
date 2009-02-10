23b8a7e497956b64a1dc9d49ec8940855141d115
Tuan  Pham <tuan.pham@exoplatform.com>
2009-02-10 04:44:05 +0000

    fix CS-2299 move skin to csResource

--- web/csResources/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++ web/csResources/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -3,21 +3,21 @@
 SkinService.addSkin(
     "web/HomePagePortlet",
     "Default",
-    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    "/csResources/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
     ServletContext
 ) ;
 
 SkinService.addSkin(
     "web/HomePagePortlet",
     "Vista",
-    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    "/csResources/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
     ServletContext
 ) ;
  
 SkinService.addSkin(
     "web/HomePagePortlet",
     "Mac",
-    "/portal/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
+    "/csResources/templates/skin/webui/component/UIHomePagePortlet/DefaultStylesheet.css",
     ServletContext
 ) ;
 
@@ -26,7 +26,7 @@ SkinService.addSkin(
 SkinService.addSkin(
     "web/BannerPortlet",
     "Default",
-    "/portal/templates/skin/webui/component/UIBannerPortlet/DefaultStylesheet.css",
+    "/csResources/templates/skin/webui/component/UIBannerPortlet/DefaultStylesheet.css",
     ServletContext,
     true
 ) ;
@@ -34,7 +34,7 @@ SkinService.addSkin(
 SkinService.addSkin(
     "web/BannerPortlet",
     "Vista",
-    "/portal/templates/skin/webui/component/UIBannerPortlet/DefaultStylesheet.css",
+    "/csResources/templates/skin/webui/component/UIBannerPortlet/DefaultStylesheet.css",
     ServletContext,
     true
 ) ;
@@ -42,7 +42,7 @@ SkinService.addSkin(
 SkinService.addSkin(
     "web/BannerPortlet",
     "Mac",
-    "/portal/templates/skin/webui/component/UIBannerPortlet/DefaultStylesheet.css",
+    "/csResources/templates/skin/webui/component/UIBannerPortlet/DefaultStylesheet.css",
     ServletContext,
     true
 ) ;
@@ -50,7 +50,7 @@ SkinService.addSkin(
 SkinService.addSkin(
     "web/FooterPortlet",
     "Default",
-    "/portal/templates/skin/webui/component/UIFooterPortlet/DefaultStylesheet.css",
+    "/csResources/templates/skin/webui/component/UIFooterPortlet/DefaultStylesheet.css",
     ServletContext,
     true
 ) ;
@@ -58,7 +58,7 @@ SkinService.addSkin(
 SkinService.addSkin(
     "web/FooterPortlet",
     "Vista",
-    "/portal/templates/skin/webui/component/UIFooterPortlet/DefaultStylesheet.css",
+    "/csResources/templates/skin/webui/component/UIFooterPortlet/DefaultStylesheet.css",
     ServletContext,
     true
 ) ;
@@ -66,7 +66,7 @@ SkinService.addSkin(
 SkinService.addSkin(
     "web/FooterPortlet",
     "Mac",
-    "/portal/templates/skin/webui/component/UIFooterPortlet/DefaultStylesheet.css",
+    "/csResources/templates/skin/webui/component/UIFooterPortlet/DefaultStylesheet.css",
     ServletContext,
     true
 ) ;
@@ -74,7 +74,7 @@ SkinService.addSkin(
 SkinService.addSkin(
     "web/NavigationPortlet",
     "Default",
-    "/portal/templates/skin/webui/component/UINavigationPorlet/DefaultStylesheet.css",
+    "/csResources/templates/skin/webui/component/UINavigationPorlet/DefaultStylesheet.css",
     ServletContext,
     true
 ) ;
@@ -82,7 +82,7 @@ SkinService.addSkin(
 SkinService.addSkin(
     "web/NavigationPortlet",
     "Vista",
-    "/portal/templates/skin/webui/component/UINavigationPorlet/DefaultStylesheet.css",
+    "/csResources/templates/skin/webui/component/UINavigationPorlet/DefaultStylesheet.css",
     ServletContext,
     true
 ) ;
@@ -90,7 +90,7 @@ SkinService.addSkin(
 SkinService.addSkin(
     "web/NavigationPortlet",
     "Mac",
-    "/portal/templates/skin/webui/component/UINavigationPorlet/DefaultStylesheet.css",
+    "/csResources/templates/skin/webui/component/UINavigationPorlet/DefaultStylesheet.css",
     ServletContext,
     true
 ) ;
