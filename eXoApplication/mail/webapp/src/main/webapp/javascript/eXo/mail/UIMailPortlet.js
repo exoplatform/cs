cf44e0929d0d9339347b7e01bebcdad7b9e90163
Hung Quang Hoang <hung.hoang@exoplatform.com>
2009-04-28 03:51:55 +0000

    cs-2567 revamp view menu

--- eXoApplication/mail/webapp/src/main/webapp/javascript/eXo/mail/UIMailPortlet.js
+++ eXoApplication/mail/webapp/src/main/webapp/javascript/eXo/mail/UIMailPortlet.js
@@ -559,8 +559,8 @@ UIMailPortlet.prototype.showView = function(obj, evt){
     this.swapMenu(uiPopupCategory, obj);
     var uiRightPopupMenuContainer = DOMUtil.findFirstDescendantByClass(this.menuElement, 'div', 'UIRightPopupMenuContainer');
     var actions = DOMUtil.findChildrenByClass(uiRightPopupMenuContainer, "a", "MenuItem");
-    actions[0].onmouseover = this.showSubmenu;
-    actions[0].onmouseout = this.hideSubmenu;
+    //actions[0].onmouseover = this.showSubmenu;
+    //actions[0].onmouseout = this.hideSubmenu;
     this.subMenu = DOMUtil.findNextElementByTagName(actions[0], "div");
     this.subMenu.style.left = actions[0].offsetWidth + "px";
     if (eXo.core.I18n.isRT()) 
@@ -568,16 +568,16 @@ UIMailPortlet.prototype.showView = function(obj, evt){
     eXo.core.DOMUtil.listHideElements(this.subMenu);
 };
 
-UIMailPortlet.prototype.showSubmenu = function(){
-    window.clearTimeout(eXo.mail.UIMailPortlet.timeOutSubmenu);
-    eXo.mail.UIMailPortlet.subMenu.style.display = "block";
-};
-
-UIMailPortlet.prototype.hideSubmenu = function(){
-    eXo.mail.UIMailPortlet.timeOutSubmenu = window.setTimeout('eXo.mail.UIMailPortlet.subMenu.style.display = "none"', 100);
-    eXo.mail.UIMailPortlet.subMenu.onmouseover = eXo.mail.UIMailPortlet.showSubmenu;
-    eXo.mail.UIMailPortlet.subMenu.onmouseout = eXo.mail.UIMailPortlet.hideSubmenu;
-};
+//UIMailPortlet.prototype.showSubmenu = function(){
+//    window.clearTimeout(eXo.mail.UIMailPortlet.timeOutSubmenu);
+//    eXo.mail.UIMailPortlet.subMenu.style.display = "block";
+//};
+//
+//UIMailPortlet.prototype.hideSubmenu = function(){
+//    eXo.mail.UIMailPortlet.timeOutSubmenu = window.setTimeout('eXo.mail.UIMailPortlet.subMenu.style.display = "none"', 100);
+//    eXo.mail.UIMailPortlet.subMenu.onmouseover = eXo.mail.UIMailPortlet.showSubmenu;
+//    eXo.mail.UIMailPortlet.subMenu.onmouseout = eXo.mail.UIMailPortlet.hideSubmenu;
+//};
 
 UIMailPortlet.prototype.swapMenu = function(oldmenu, clickobj){
     var DOMUtil = eXo.core.DOMUtil;
