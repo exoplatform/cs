f7113066ec6fe35f8f3d7d994c2b86d181529d9e
Tuan  Pham <tuan.pham@exoplatform.com>
2009-04-28 05:33:05 +0000

    CS-2531 harset used to read xml rss is always utf-8, CS-2624 Check new parent pom 1.1.1 to be used in trunks and new releases

--- eXoApplication/content/service/src/main/java/org/exoplatform/content/service/RSSContentPlugin.java
+++ eXoApplication/content/service/src/main/java/org/exoplatform/content/service/RSSContentPlugin.java
@@ -7,6 +7,9 @@ package org.exoplatform.content.service;
 import java.net.URL;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.exoplatform.commons.utils.PageList;
 import org.exoplatform.content.model.ContentItem;
 import org.exoplatform.content.model.ContentNode;
@@ -14,6 +17,7 @@ import org.exoplatform.services.rss.parser.DefaultRSSChannel;
 import org.exoplatform.services.rss.parser.DefaultRSSItem;
 import org.exoplatform.services.rss.parser.RSSDocument;
 import org.exoplatform.services.rss.parser.RSSParser;
+import org.w3c.dom.Document;
 
 /**
  * Created by The eXo Platform SARL
@@ -33,9 +37,14 @@ public class RSSContentPlugin extends ContentPlugin {
 
   @SuppressWarnings("unchecked")
   public PageList loadContentMeta(ContentNode node) throws Exception {
+    //TODO: tuan.pham CS-2531 get encode from rss file
+    DocumentBuilder  docbuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder() ;
+    Document doc = docbuilder.parse(node.getUrl()) ;
+    String encode = doc.getXmlEncoding() ;
+    if (encode == null || encode.trim().length() == 0) encode = "utf-8" ;
     URL uri = new URL(node.getUrl()); 
     RSSDocument<DefaultRSSChannel, RSSItem> document = 
-      service_.createDocument(uri, "utf-8", DefaultRSSChannel.class, RSSItem.class);
+      service_.createDocument(uri, encode, DefaultRSSChannel.class, RSSItem.class);
     List<RSSItem> list = document.getItems();     
     return new ContentPageList(list);
   } 
@@ -52,5 +61,5 @@ public class RSSContentPlugin extends ContentPlugin {
     }
 
   }
-  
+
 }
