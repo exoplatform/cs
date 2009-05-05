a5b1e6bb41b52172daeab071601fa5d0da633a20
Viet Tien Nguyen <viet.nguyen@exoplatform.com>
2009-05-05 06:44:05 +0000

    make Layout Tab in Mail Settings (CS-2568)

---
+++ eXoApplication/mail/webapp/src/main/java/org/exoplatform/mail/webui/popup/UIMailLayoutTab.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.mail.webui.popup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.core.model.SelectItemOption;
+import org.exoplatform.webui.form.UIForm;
+import org.exoplatform.webui.form.UIFormInputSet;
+import org.exoplatform.webui.form.UIFormRadioBoxInput;
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : eXoPlatform
+ *          exo@exoplatform.com
+ * May 4, 2009  
+ */
+
+@ComponentConfig(
+  template = "app:/templates/mail/webui/popup/UIMailLayoutTab.gtmpl"
+) 
+              
+              
+public class UIMailLayoutTab extends UIFormInputSet {
+
+  public static final String VERTICAL_LAYOUT = "UIMailSettings-VerticalLayout";
+  public static final String HORIZONTAL_LAYOUT= "UIMailSettings-HorizontalLayout";
+  public static final String NOSPLIT_LAYOUT = "UIMailSettings-NosplitLayout";
+  public static final String VERTICAL_LAYOUT_OPTION = "vertical-layout";
+  public static final String HORIZONTAL_LAYOUT_OPTION = "horizontal-layout";
+  public static final String NOSPLIT_LAYOUT_OPTION = "nosplit-layout";
+  public static final String RADIO_ID = "group-id";
+  
+  
+  public UIMailLayoutTab(String id) throws Exception {
+    super(id);
+    setComponentConfig(getClass(), null) ;
+
+    List<SelectItemOption<String>> layoutOption = new ArrayList<SelectItemOption<String>>();
+    layoutOption.add(new SelectItemOption<String>(VERTICAL_LAYOUT_OPTION,VERTICAL_LAYOUT_OPTION));
+    UIFormRadioBoxInput uiFormRadioBoxInput=new UIFormRadioBoxInput(RADIO_ID,VERTICAL_LAYOUT_OPTION,layoutOption);
+    uiFormRadioBoxInput.setId(VERTICAL_LAYOUT);
+    addUIFormInput(uiFormRadioBoxInput);
+    layoutOption = new ArrayList<SelectItemOption<String>>();
+    layoutOption.add(new SelectItemOption<String>(HORIZONTAL_LAYOUT_OPTION,HORIZONTAL_LAYOUT_OPTION));
+    uiFormRadioBoxInput=new UIFormRadioBoxInput(RADIO_ID,HORIZONTAL_LAYOUT_OPTION,layoutOption);
+    uiFormRadioBoxInput.setId(HORIZONTAL_LAYOUT);
+    addUIFormInput(uiFormRadioBoxInput);
+    layoutOption = new ArrayList<SelectItemOption<String>>();
+    layoutOption.add(new SelectItemOption<String>(NOSPLIT_LAYOUT_OPTION,NOSPLIT_LAYOUT_OPTION));
+    uiFormRadioBoxInput=new UIFormRadioBoxInput(RADIO_ID,NOSPLIT_LAYOUT_OPTION,layoutOption);
+    uiFormRadioBoxInput.setId(NOSPLIT_LAYOUT);
+    addUIFormInput(uiFormRadioBoxInput);
+    //TODO: read from database
+  }
+  
+ 
+  protected UIForm getParentFrom() {
+    return (UIForm)getParent() ;
+  }
+}
