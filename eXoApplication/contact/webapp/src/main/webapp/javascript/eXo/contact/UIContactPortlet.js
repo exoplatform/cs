90dc492b01b8f0865ee3165de94cbac9209697b7
Hung Quang Hoang <hung.hoang@exoplatform.com>
2009-08-04 08:16:32 +0000

    CS-3028 action on address book

--- eXoApplication/contact/webapp/src/main/webapp/javascript/eXo/contact/UIContactPortlet.js
+++ eXoApplication/contact/webapp/src/main/webapp/javascript/eXo/contact/UIContactPortlet.js
@@ -198,7 +198,7 @@ UIContactPortlet.prototype.addressBookCallback = function(evt) {
 	var _e = window.event || evt ;
 	var src = _e.srcElement || _e.target ;
 	var addressBook = (DOMUtil.hasClass(src, "ItemList")) ? src : DOMUtil.findAncestorByClass(src, "ItemList") ;	
-	var menuItems = DOMUtil.findDescendantsByClass(UIContextMenuCon.menuElement, "div", "ItemIcon") ;
+	var menuItems = DOMUtil.findDescendantsByClass(UIContextMenuCon.menuElement, "a", "ItemIcon") ;
 	var itemLength = menuItems.length ;	
 
 	var isDefault = addressBook.getAttribute("isDefault") ;
@@ -206,21 +206,21 @@ UIContactPortlet.prototype.addressBookCallback = function(evt) {
 		if (isDefault == "true") {
 			for(var i = 0 ; i < itemLength ; i ++) {
 				if (DOMUtil.hasClass(menuItems[i],"DeleteIcon")) {
-					if (menuItems[i].parentNode.getAttribute("oldHref")) continue ;
-					menuItems[i].parentNode.setAttribute("oldHref", menuItems[i].parentNode.href) ;
-					menuItems[i].parentNode.href = "javascript: void(0) ;" ;
-					menuItems[i].parentNode.setAttribute("oldColor", DOMUtil.getStyle(menuItems[i].parentNode, "color")) ;
-					menuItems[i].parentNode.style.color = "#cccccc" ;
+					if (menuItems[i].getAttribute("oldHref")) continue ;
+					menuItems[i].setAttribute("oldHref", menuItems[i].href) ;
+					menuItems[i].href = "javascript: void(0) ;" ;
+					menuItems[i].setAttribute("oldColor", DOMUtil.getStyle(menuItems[i], "color")) ;
+					menuItems[i].style.color = "#cccccc" ;
 				}
 			}
 		} else { // isDefault = false
 	    for(var i = 0 ; i < itemLength ; i ++) {
 				if (DOMUtil.hasClass(menuItems[i],"DeleteIcon")) {
-					if (!menuItems[i].parentNode.getAttribute("oldHref")) continue ;
-					menuItems[i].parentNode.href = menuItems[i].parentNode.getAttribute("oldHref") ;
-					menuItems[i].parentNode.style.color = menuItems[i].parentNode.getAttribute("oldColor") ;
-					menuItems[i].parentNode.removeAttribute("oldColor") ;
-					menuItems[i].parentNode.removeAttribute("oldHref") ;
+					if (!menuItems[i].getAttribute("oldHref")) continue ;
+					menuItems[i].href = menuItems[i].getAttribute("oldHref") ;
+					menuItems[i].style.color = menuItems[i].getAttribute("oldColor") ;
+					menuItems[i].removeAttribute("oldColor") ;
+					menuItems[i].removeAttribute("oldHref") ;
 				}
 			}
   	}
@@ -230,22 +230,22 @@ UIContactPortlet.prototype.addressBookCallback = function(evt) {
 			for(var i = 0 ; i < itemLength ; i ++) {
 				if (DOMUtil.hasClass(menuItems[i],"ContactIcon") || DOMUtil.hasClass(menuItems[i],"PasteIcon")
 					|| DOMUtil.hasClass(menuItems[i],"EditActionIcon") || DOMUtil.hasClass(menuItems[i],"ImportContactIcon")) {
-					if (menuItems[i].parentNode.getAttribute("oldHref")) continue ;
-					menuItems[i].parentNode.setAttribute("oldHref", menuItems[i].parentNode.href) ;
-					menuItems[i].parentNode.href = "javascript: void(0) ;" ;
-					menuItems[i].parentNode.setAttribute("oldColor", DOMUtil.getStyle(menuItems[i].parentNode, "color")) ;
-					menuItems[i].parentNode.style.color = "#cccccc" ;
+					if (menuItems[i].getAttribute("oldHref")) continue ;
+					menuItems[i].setAttribute("oldHref", menuItems[i].href) ;
+					menuItems[i].href = "javascript: void(0) ;" ;
+					menuItems[i].setAttribute("oldColor", DOMUtil.getStyle(menuItems[i], "color")) ;
+					menuItems[i].style.color = "#cccccc" ;
 				}
 			}		
 		} else {
 			for(var i = 0 ; i < itemLength ; i ++) {
 				if (DOMUtil.hasClass(menuItems[i],"ContactIcon") || DOMUtil.hasClass(menuItems[i],"PasteIcon")
 					|| DOMUtil.hasClass(menuItems[i],"EditActionIcon") || DOMUtil.hasClass(menuItems[i],"ImportContactIcon")) {
-					if (!menuItems[i].parentNode.getAttribute("oldHref")) continue ;
-					menuItems[i].parentNode.href = menuItems[i].parentNode.getAttribute("oldHref") ;
-					menuItems[i].parentNode.style.color = menuItems[i].parentNode.getAttribute("oldColor") ;
-					menuItems[i].parentNode.removeAttribute("oldColor") ;
-					menuItems[i].parentNode.removeAttribute("oldHref") ;
+					if (!menuItems[i].getAttribute("oldHref")) continue ;
+					menuItems[i].href = menuItems[i].getAttribute("oldHref") ;
+					menuItems[i].style.color = menuItems[i].getAttribute("oldColor") ;
+					menuItems[i].removeAttribute("oldColor") ;
+					menuItems[i].removeAttribute("oldHref") ;
 				}
 			}		
 		} 
