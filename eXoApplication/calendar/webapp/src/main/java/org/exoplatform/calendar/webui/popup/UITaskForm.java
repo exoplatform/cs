a4e30a2ac6396c9c940c707703cf0296210b432c
Hung Quang Hoang <hung.hoang@exoplatform.com>
2010-02-08 06:54:42 +0000

    CS-2731 fix bug after optimize query

--- eXoApplication/calendar/webapp/src/main/java/org/exoplatform/calendar/webui/popup/UITaskForm.java
+++ eXoApplication/calendar/webapp/src/main/java/org/exoplatform/calendar/webui/popup/UITaskForm.java
@@ -662,6 +662,7 @@ public class UITaskForm extends UIFormTabPane implements UIPopupComponent, UISel
     taskDetailTab.refreshUploadFileList() ;
   }
   protected void setEventReminders(List<Reminder> reminders){
+    if (reminders == null) return;
     UIEventReminderTab taskDetailTab =  getChildById(TAB_TASKREMINDER) ;
     for(Reminder r : reminders) {
       if(Reminder.TYPE_EMAIL.equals(r.getReminderType())) {
