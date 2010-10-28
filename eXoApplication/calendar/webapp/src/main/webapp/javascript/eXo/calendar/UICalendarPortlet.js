0682835ffdda2f8094792876f25a41959c1a4285
Quang Thanh Le <quang.le@exoplatform.com>
2010-10-28 04:53:34 +0000

    CS-4627: fix bug that do not allow user write specified time when adding event.

--- eXoApplication/calendar/webapp/src/main/webapp/javascript/eXo/calendar/UICalendarPortlet.js
+++ eXoApplication/calendar/webapp/src/main/webapp/javascript/eXo/calendar/UICalendarPortlet.js
@@ -2784,6 +2784,8 @@ UICombobox.prototype.correct = function() {
 	var UICombobox = eXo.webui.UICombobox ; 
 	var value = this.value ;
 	this.value = UICombobox.setValue(value) ;
+	var hiddenField = eXo.core.DOMUtil.findPreviousElementByTagName(comboList[i],"input");
+	hiddenField.value = this.value;
 } ;
 
 UICombobox.prototype.setValue = function(value) {
