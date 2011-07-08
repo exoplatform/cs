98774e53d3a684fe793c3c0a429bb2137dbb5dbc
Viet Tien Nguyen <viet.nguyen@exoplatform.com>
2011-07-08 07:24:23 +0000

    CS-5087: Upgrade to EXOJCR 1.14.0-CR3

--- eXoApplication/mail/service/src/test/java/org/exoplatform/mail/service/test/FakeJobSchedulerServiceImpl.java
+++ eXoApplication/mail/service/src/test/java/org/exoplatform/mail/service/test/FakeJobSchedulerServiceImpl.java
@@ -168,14 +168,20 @@ public class FakeJobSchedulerServiceImpl implements JobSchedulerService {
 
   @Override
   public void addCronJob(ComponentPlugin plugin) throws Exception {
-    // TODO Auto-generated method stub
-    
   }
 
   @Override
   public void addPeriodJob(ComponentPlugin plugin) throws Exception {
-    // TODO Auto-generated method stub
-    
+  }
+
+  @Override
+  public boolean suspend() {
+    return false;
+  }
+
+  @Override
+  public boolean resume() {
+    return false;
   }
 
 }
