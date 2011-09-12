e90f4980a1b13d5438d557dfe66461db616e8da3
Quang Thanh Le <quang.le@exoplatform.com>
2011-09-12 09:27:26 +0000

    CS-5307: [CS] Gadget MyTasks request rest service sequential cause portal's time out function doesn't work         * remove 'portal' term in rest links

--- web/csResources/src/main/webapp/gadgets/tasks/skin/tasks.js
+++ web/csResources/src/main/webapp/gadgets/tasks/skin/tasks.js
@@ -120,7 +120,7 @@ function eXoEventGadget(){
 
 eXoEventGadget.prototype.getPrefs = function(){
 	var setting = (new gadgets.Prefs()).getString("setting");
-	if(setting =="") setting = ["","/portal/rest/cs/calendar/getissues","10","AM/PM","defaultCalendarName"];
+	if(setting =="") setting = ["","/rest/cs/calendar/getissues","10","AM/PM","defaultCalendarName"];
 	else {
 		setting = setting.split(";");
 	}
@@ -145,7 +145,7 @@ eXoEventGadget.prototype.setLink = function(){
 eXoEventGadget.prototype.createRequestUrl = function(){
 	var prefs = eXoEventGadget.prefs || eXoEventGadget.getPrefs();
 	var limit = (prefs.limit && (parseInt(prefs.limit) > 0))? prefs.limit:0;
-	var subscribeurl = (prefs.subscribeurl)?prefs.subscribeurl: "/portal/rest/cs/calendar/getissues" ;
+	var subscribeurl = (prefs.subscribeurl)?prefs.subscribeurl: "/rest/cs/calendar/getissues" ;
 	subscribeurl +=  "/" + DateTimeFormater.format((new Date()),"yyyymmdd") + "/Task/" + limit ;
 	subscribeurl += "?rnd=" + (new Date()).getTime();
 	return subscribeurl;
